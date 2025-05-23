c8fb90c4d4cdebeb53028cbdc906b137e5818a45
Vinicius Beda <vinicius.beda05@gmail.com>
2025-05-22 23:34:18 -0300

    reajustando o projeto

--- Models/Moto.cs
+++ Models/Moto.cs
@@ -2,9 +2,16 @@ namespace MottuSmartPatio.Models
 {
     public class Moto
     {
+        /// <example>1</example>
         public int Id { get; set; }
+
+        /// <example>ABC1234</example>
         public string Placa { get; set; } = string.Empty;
+
+        /// <example>Honda CG 160</example>
         public string Modelo { get; set; } = string.Empty;
+
+        /// <example>Ativa</example>
         public string Status { get; set; } = string.Empty;
     }
 }
